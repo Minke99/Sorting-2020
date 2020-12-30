70438431974d1df741de7de6d62c2789b4d51b66
KNN-6948 <1007426372@qq.com>
2020-12-30 10:41:15 +0800

    add main.c main.h

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include'main.h'
+void sort(int* arr, int len)
+{
+	merge_sort();
+	pivot_sort();
+}
+int main()
+{
+    int arr[] = {7, 6, 2, 1, 4, 9, 3};
+    //len is the number of element
+    int len = sizeof(arr)/sizeof(int);
+    printf("len = %d\n, then call the function of sort", len);
+    sort(arr,len);
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,3 @@
+void merge_sort();
+void pivot_sort();
+#define pi 3.14
