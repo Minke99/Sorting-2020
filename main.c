0b7aa128963ca2bd727e256388b5aaae5a0ed14d
KNN-6948 <1007426372@qq.com>
2021-01-23 14:26:33 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include"main.h"
 void sort(int* arr, int len);
-
+// Here is the start of the all programs.
+// Function main init the arr and send it to the function sort with its length.
 int main()
 {
     int arr[] = {7, 6, 2, 1, 4, 9, 3};
@@ -12,6 +13,10 @@ int main()
     return 0;
     printf("sgg has modified this file-2021.1.13");
 }
+// It provide 2 potral of the sort functions, merge and pivot.
+// I did not init the input for them, try yourself in the other scripts and declear them here.
+// You cannot use them at the same run.
+// Comment one of them and run(shown below).
 void sort(int* arr, int len)
 {
 	printf("Start merge sort now\n");
